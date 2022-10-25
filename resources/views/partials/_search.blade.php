426be07a9762d03b11355c78a54143810e766cbc
Alex Chiew <alex.chiewyz@gmail.com>
2022-10-26 02:16:07 +0800

    Fix search bar in positioned weirdly and refactor class selectors

--- resources/views/partials/_search.blade.php
+++ resources/views/partials/_search.blade.php
@@ -1,11 +1,9 @@
-<div class="header__nav-search-container header__flex">
-    <form class="header__nav-search-wrapper header__flex" action="" method="POST" role="search">
-        @csrf
-        <div class="header__nav-search-bar">
-            <input type="search" class="search-term" placeholder="Search for products..." aria-label="Search" />
-            <button type="submit" class="header__nav-search-btn" aria-label="Search">
-                <x-svg.icons.homepage.navbar.search class="search-icon" />
-            </button>
-        </div>
-    </form>
-</div>
+<form class="search-wrapper" action="" method="POST" role="search">
+    @csrf
+    <div class="search-bar">
+        <input type="search" class="search-term" placeholder="Search for products..." aria-label="Search" />
+        <button type="submit" class="search-btn" aria-label="Search">
+            <x-svg.icons.homepage.navbar.search class="search-icon" />
+        </button>
+    </div>
+</form>

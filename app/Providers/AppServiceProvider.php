1b577d0178e5bf372a87ba40d51b7160318fc467
Alex Chiew <alex.chiewyz@gmail.com>
2022-10-26 02:08:28 +0800

    Add Pagination bootstrap five

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Pagination\Paginator;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Paginator::useBootstrapFive();
+
         if (config('app.env') === 'production') {
             URL::forceScheme('https');
         };

66c53beea11cd0cc3eb0f5517a3d4d4d209eb166
Alex Chiew <alex.chiewyz@gmail.com>
2022-12-04 00:44:53 +0800

    Modify product view

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -3,11 +3,24 @@
 @section('title', $product->name)
 
 @section('extra-css')
-    <link rel="stylesheet" href="{{ asset('css/algolia.css') }}">
+<link rel="stylesheet" href="{{ asset('css/algolia.css') }}">
 @endsection
 
 @section('content')
-    <x-breadcrumbs>
-        
-    </x-breadcrumbs>
+<x-breadcrumbs>
+    <div class="breadcrumbs__text-container">
+        <h4>Shop</h4>
+        <div class="breadcrumbs__text breadcrumbs__flex">
+            <a class="breadcrumbs__text-home" href="{{ route('home') }}">Home</a>
+            <span class="breadcrumbs__separator breadcrumbs__flex">
+                <x-svg.icons.shop.breadcrumbs class="angle-right-icon" />
+            </span>
+            <a class="breadcrumbs__text-home" href="{{ route('shop.index') }}">Shop</a>
+            <span class="breadcrumbs__separator breadcrumbs__flex">
+                <x-svg.icons.shop.breadcrumbs class="angle-right-icon" />
+            </span>
+            <span class="breadcrumbs__text-shop breadcrumbs__flex">{{ $product->name }}</span>
+        </div>
+    </div>
+</x-breadcrumbs>
 @endsection
